9c0f3444a3b03a8bf5f5b2502dab80bee5d0262b
zombie110year <zombie110year@outlook.com>
2019-02-21 19:40:49 +0800

    修复 GenerateConfigAction 的实例化

--- drcom/config.py
+++ drcom/config.py
@@ -1,12 +1,11 @@
-from argparse import ArgumentParser
-from pathlib import Path
+from argparse import SUPPRESS, Action, ArgumentParser
 from os import environ
+from pathlib import Path
 from pkgutil import get_data
-from .utils import Namespace
 from platform import system
 from sys import exit
 
-from argparse import Action
+from .utils import Namespace
 
 DEFAULT_CONFIG_FILES = [
     Path("./drcom.conf"),
@@ -30,6 +29,17 @@ class SetFilesPathAction(Action):
 class GenerateFileAction(Action):
     """生成文件后退出
     """
+    def __init__(self,
+                 option_strings,
+                 dest=SUPPRESS,
+                 default=SUPPRESS,
+                 help=None):
+        super(GenerateFileAction, self).__init__(
+            option_strings=option_strings,
+            dest=dest,
+            default=default,
+            nargs=0,
+            help=help)
     def __call__(self, parser, namespace, values, option_string=None):
         file = get_data(__package__, "drcom.conf.temp").decode("utf-8")
         with open("./drcom.conf", "wt", encoding="utf-8") as target:
@@ -53,7 +63,6 @@ def getCliArgs():
     parser.add_argument(
         "--generate-config",
         help="在当前目录下生成配置文件模板",
-        required=False,
         action=GenerateFileAction,
         default=False
     )
@@ -90,4 +99,3 @@ def configure():
     arg = getCliArgs()
     conf = getConfigFileContent(arg.config)
     return conf
-
