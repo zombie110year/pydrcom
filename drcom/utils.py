33b0dc8bd9f70e2c845eba45ad1f4464c8e517f4
zombie110year <zombie110year@outlook.com>
2019-03-12 00:33:44 +0800

    utils.getIP: 手动关闭 socket

--- drcom/utils.py
+++ drcom/utils.py
@@ -112,6 +112,7 @@ def getIP():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(('8.8.8.8', 80))
     ip, port = s.getsockname()
+    s.close()
     return ip
 
 def getMacAdress():
