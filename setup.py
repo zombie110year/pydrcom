74d993a1b4a9cc846cfaa0699ef0ffcf23680f7e
zombie110year <zombie110year@outlook.com>
2019-08-29 14:50:54 +0800

    :bug: 内部的 toml 未被打包

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 PROJECT_NAME = "pydrcom"
-VERSION = "3.0.0"
+VERSION = "3.0.1"
 DESCRIPTION = open("docs/_static/description", "rt", encoding="utf-8").read()
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     description=DESCRIPTION,
-    packages=['drcom'],
+    packages=['drcom', 'drcom.toml'],
     entry_points={
         "console_scripts": [
             "drcom = drcom.__init__:main"
